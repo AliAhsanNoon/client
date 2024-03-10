3fb2ed526719e0cde0e6546993802c3824420417
alinoon <alinoonciit@gmail.com>
2024-03-10 18:19:23 +0500

    added client app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  /**
+   *
+   */
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get("http://localhost:5000/api/user").subscribe(
+      {
+        next: response => console.log(response),
+        error: error => console.log('getting error on fetch users', error),
+        complete: () => console.log('http_request_completed')
+      }
+
+    )
+  }
   title = 'client';
 }
